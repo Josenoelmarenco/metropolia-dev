b9c18aa11accc099633ba26a623b3b6ad609b900
José Noel Marenco <45679964+Josenoelmarenco@users.noreply.github.com>
2026-01-19 16:32:04 +0200

    Entrega Week 1 y Week 2

---
+++ week2/fe-lab1/intro-react-project/src/App.jsx
@@ -0,0 +1,14 @@
+import Hello from "./Hello";
+import Bye from "./Bye";
+
+//Primero debo exportar a app prácticamente.
+function App() {
+  return (
+    <div>
+      <Hello />
+      <Bye />
+    </div>
+  );
+}
+
+export default App;
---
+++ week2/week2-fe-lab4/src/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <div className="header">
+        <header>
+        <h1>Welcome to my version of React Lab</h1>
+        </header>
+
+    </div>
+  );
+}
+
+export default Header;
