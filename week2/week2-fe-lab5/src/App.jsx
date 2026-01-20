24883950d5b3cb5e51f7a67c2cdcd601d59bfbf3
José Noel Marenco <45679964+Josenoelmarenco@users.noreply.github.com>
2026-01-20 21:12:19 +0200

    Update, is done lab3 - lab7

---
+++ week2/week2-fe-lab5/src/App.jsx
@@ -0,0 +1,17 @@
+// App.jsx
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MainContent from './components/MainContent';
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ week2/week2-fe-lab5/src/components/MainContent.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function MainContent(){
+    return(
+        <div className="main">
+            <section className='section1'>
+                <h2>Section 1</h2>
+                <p>This is the content of section 1.</p>
+            </section>
+            <section className='section2'>
+                <h2>Section 2</h2>
+                <p>This is the content of section 2.</p>
+            </section>
+        </div>
+    );
+}
+
+export default MainContent
