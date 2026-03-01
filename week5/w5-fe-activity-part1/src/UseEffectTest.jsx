b11f1dd0e54eb41ff5e8da14757e75d0f0fceac0
José Noel Marenco <45679964+Josenoelmarenco@users.noreply.github.com>
2026-03-01 20:22:42 +0200

    activity 1/3 complete

--- week5/w5-fe-activity-part1/src/UseEffectTest.jsx
+++ week5/w5-fe-activity-part1/src/UseEffectTest.jsx
@@ -3,16 +3,41 @@ import {useEffect, useState } from 'react';
 
 const UseEffectTest = () => {
     const [toggleOne, setToggleOne] = useState(false);
+    const [toggleTwo, setToggleTwo] = useState(false);
+    const [count, setCount] = useState(0);
 
     useEffect(() => {
         console.log('UseEffect1 Ran 😃');
     }, []);
+
+    useEffect(() => {
+    console.log('UseEffect2 Ran 🥳');
+    if (toggleTwo)
+        console.log('toggleTwo slice of state is true so this code runs');
+    }, [toggleTwo]); // <-- ¡Aquí le decimos que escuche solo a toggleTwo!
+
+    useEffect(() => {
+    // 1. Asignamos el setInterval a una variable llamada myInterval
+    const myInterval = setInterval(() => {
+      console.log(`UseEffect3 with interval number ${count} is running`);
+    }, 1000);
+
+    // 2. Retornamos la función de limpieza
+    return () => {
+      console.log(
+        `UseEffect3 cleanup ran.\nsetInterval number ${count} is being cleared out`
+      );
+      clearInterval(myInterval);
+    };
+  }, [count]);
 
   return (
     <div>
         {console.log('rendered or re-rendered 😵‍💫')}
         <h1>UseEffectTest Component</h1>
         <button onClick={() => setToggleOne(!toggleOne)}>ToggleOne</button>
+        <button onClick={() => setToggleTwo(!toggleTwo)}>ToggleTwo</button>
+        <button onClick={() => setCount(count + 1)}>Count</button>
     </div>
   );
 };
