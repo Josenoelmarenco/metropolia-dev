00f10798489914f126df853362514e84aad32aa1
José Noel Marenco <45679964+Josenoelmarenco@users.noreply.github.com>
2026-03-01 19:41:04 +0200

    w5-fe-actitivy-part 1

---
+++ week5/w5-fe-activity-part1/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import UseEffectTest from './UseEffectTest'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <UseEffectTest />        
+    </>
+  )
+}
+
+export default App
